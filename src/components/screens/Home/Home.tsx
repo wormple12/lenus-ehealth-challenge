a6005ec6215fffbcbe536390a3e90fba56687636
wormple12 <wormple12@hotmail.com>
2021-09-23 13:49:36 +0200

    recreated getMeasurements as a real-time updating stream + initiated work and planning for the UI + downgraded to React 16 to avoid issues with packages not yet up-to-speed

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+
+import { Measurement } from '@Types/Measurement';
+import chartDatabase from '@Services/firebase/chartDatabase';
+
+import './Home.scss';
 
 type Props = RouteComponentProps;
 
 export const Home: React.FC<Props> = props => {
+    const [measurements, setMeasurements] = useState<Measurement[]>([]);
+
+    useEffect(() => {
+        const unsubscribe = chartDatabase.streamMeasurements(setMeasurements);
+        return () => {
+            const cleanup = async () => await unsubscribe;
+            cleanup();
+        };
+    }, [setMeasurements]);
 
     return (
         <section className="content">
-            <h1></h1>
-            <p className="sectionDesc"></p>
+            <h1>Weight Tracker</h1>
+            <p className="sectionDesc">
+                This is a simple proof-of-concept app allowing a single person to track their weight over time.
+                Simply log your measurements, and both the list and the chart below will automatically be updated.
+            </p>
             <br />
+            <div className="home-page-flex">
+                <div id="chart-container"></div>
+                <div id="list-container">
+                    <h2>Measurements</h2>
+                    {/* <List>
+                        {measurements.map(m => (
+                            <ListItem
+                                caption={m.date.toDateString()}
+                                legend={`${m.weight} kg`}
+                                rightIcon='delete'
+                            />
+                        ))}
+                    </List> */}
+                </div>
+            </div>
         </section>
     );
 };
--- src/services/firebase/chartDatabase.spec.ts
+++ src/services/firebase/chartDatabase.spec.ts
@@ -1,27 +1,29 @@
+import { DocumentData, QuerySnapshot } from '@firebase/firestore';
+import { Measurement } from '@Types/Measurement';
 import ChartDatabase, * as db_methods from './chartDatabase';
 
 /**
- * Helper method to stub the database fetching method to instead return the
- * given data, mimicking the original firestore method's response.
+ * Helper method to get an object that mimics a Firestore snapshot without
+ * actually being one.
  */
-const stubDatabaseCall = (testData) => {
-    cy.stub(db_methods, 'readFromStore').resolves({
+const getSnapshotMimic = (testData: Measurement[]) => {
+    return {
         docs: testData.map(m => {
             return {
                 id: m.date,
                 data: () => m
             };
         })
-    });
+    };
 };
 
 export default function chartDatabase_test() {
-    describe('ChartDatabase.getMeasurements', () => {
+    describe('transforming firestore query snapshot to measurements', () => {
         it('should return correct data as Measurement[]', () => {
             cy.fixture('measurements').as('measurementsJson').then(async testData => {
-                stubDatabaseCall(testData);
+                // @ts-ignore: let the snapshot mimic bypass type checking
+                const measurements = db_methods.transformSnapshotToMeasurements(getSnapshotMimic(testData));
 
-                const measurements = await ChartDatabase.getMeasurements();
                 expect(measurements).to.have.lengthOf(4);
                 expect(measurements[0]).to.have.all.keys('date', 'weight');
                 expect(measurements[1].date.toDateString()).to.equal(
